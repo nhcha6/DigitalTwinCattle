0e97d8bc23a75af192a5390dff77ad7c111b0432
nhcha6 <nhcha6@student.monash.edu>
2020-12-18 16:44:41 +1100

    plotting for comparison of filter parameters

--- filter_data.py
+++ filter_data.py
@@ -72,51 +72,58 @@ def fourier_transform(signal, title):
     plt.ylabel("Magnitude")
 #print(pywt.wavelist())
 
-def fil_lp_filter(cutoff_hz, width_hz, signal, signal_name):
-    # sample rate is 24 per day
-    sample_rate = 24
+def fil_lp_filter(cutoff_hz_list, width_hz_list, signal, signal_name):
+    i = 0
+    plt.figure(figsize=(len(width_hz_list)*10, len(cutoff_hz_list)*4))
+    plt.suptitle("Low Pass vs Unfiltered Signal (" + signal_name + ")")
+    for cutoff_hz in cutoff_hz_list:
+        for width_hz in width_hz_list:
+            i+=1
+            # sample rate is 24 per day
+            sample_rate = 24
 
-    # The Nyquist rate of the signal.
-    nyq_rate = sample_rate / 2.0
-    t = np.arange(len(signal)) / sample_rate
+            # The Nyquist rate of the signal.
+            nyq_rate = sample_rate / 2.0
+            t = np.arange(len(signal)) / sample_rate
 
-    # The desired width of the transition from pass to stop,
-    # relative to the Nyquist rate.
-    width = width_hz/nyq_rate
+            # The desired width of the transition from pass to stop,
+            # relative to the Nyquist rate.
+            width = width_hz/nyq_rate
 
-    # The desired attenuation in the stop band, in dB.
-    ripple_db = 60.0
+            # The desired attenuation in the stop band, in dB.
+            ripple_db = 60.0
 
-    # Compute the order and Kaiser parameter for the desired FIR filter.
-    N, beta = kaiserord(ripple_db, width)
+            # Compute the order and Kaiser parameter for the desired FIR filter.
+            N, beta = kaiserord(ripple_db, width)
 
-    # Use firwin with a Kaiser window to create a lowpass FIR filter. Taps are
-    # the coefficients for the filter
-    taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
+            # Use firwin with a Kaiser window to create a lowpass FIR filter. Taps are
+            # the coefficients for the filter
+            taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
 
-    # Use lfilter to filter x with the FIR filter.
-    filtered_signal = lfilter(taps, 1.0, signal)
+            # Use lfilter to filter x with the FIR filter.
+            filtered_signal = lfilter(taps, 1.0, signal)
 
-    # The phase delay of the filtered signal.
-    delay = 0.5 * (N-1) / sample_rate
+            # The phase delay of the filtered signal.
+            delay = 0.5 * (N-1) / sample_rate
 
-    # plot filter response
-    # plt.figure()
-    # w, h = freqz(taps, worN=8000)
-    # plt.plot((w / np.pi) * nyq_rate, np.absolute(h), linewidth=2)
-    # plt.xlabel('Frequency (Hz)')
-    # plt.ylabel('Gain')
-    # plt.title('Filter Frequency Response')
-    # plt.ylim(-0.05, 1.05)
-    # plt.grid(True)
+            # plot filter response
+            # plt.figure()
+            # w, h = freqz(taps, worN=8000)
+            # plt.plot((w / np.pi) * nyq_rate, np.absolute(h), linewidth=2)
+            # plt.xlabel('Frequency (Hz)')
+            # plt.ylabel('Gain')
+            # plt.title('Filter Frequency Response')
+            # plt.ylim(-0.05, 1.05)
+            # plt.grid(True)
 
-    plt.figure()
-    # Plot the original signal.
-    plt.plot(t, signal, label='signal')
-    # Plot the filtered signal, shifted to compensate for the phase delay.
-    plt.plot(t-delay, filtered_signal, 'r-', label = 'low pass')
-    # Plot just the "good" part of the filtered signal.  The first N-1
-    # samples are "corrupted" by the initial conditions.
-    plt.xlabel('days')
-    plt.title('Low Pass vs Unfiltered Signal, cutoff = ' + str(cutoff_hz) + 'Hz, width = ' + str(width_hz) + 'Hz (' + signal_name + ')', fontsize=10)
-    plt.grid(True)
+
+            # Plot the original signal
+            plt.subplot(len(cutoff_hz_list), len(width_hz_list), i)
+            plt.plot(t, signal, label='signal')
+            # Plot the filtered signal, shifted to compensate for the phase delay.
+            plt.plot(t-delay, filtered_signal, 'r-', label = 'low pass')
+            # Plot just the "good" part of the filtered signal.  The first N-1
+            # samples are "corrupted" by the initial conditions.
+            plt.xlabel('days')
+            plt.title('Cutoff = ' + str(cutoff_hz) + 'Hz, Width = ' + str(width_hz) + 'Hz', fontsize=10)
+            plt.tight_layout()
