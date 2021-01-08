e108fc22e9d95b6ef5b1e477124db41d236789fb
nhcha6 <nhcha6@student.monash.edu>
2021-01-08 14:02:47 +1100

    code for generating time series across 73 days created

--- Regressions/regression_master.py
+++ Regressions/regression_master.py
@@ -6,6 +6,9 @@ from hot_day_trends import *
 from filter_data import *
 from area_under_curve import *
 
+# df = pd.read_csv("panting_regression.csv")
+# print(df)
+
 ###################### ESTABLISH REQUIRED DATA ##################
 
 # default setting of these variables
@@ -28,10 +31,13 @@ docility_dict = create_category_dict("Docility score", "Tag#", cow_details_df)
 # plot_cows = [str(x) for x in coat_dict["Black"]]
 
 # or we can manually select cattle
-cow_category = "8027107 - White, 50%, F"
-plot_cows = ['8027107']
+#cow_category = "8027107 - White, 50%, F"
+#plot_cows = ['8027107']
 # cow_category = "8022092 - Red, 39%, F"
 # plot_cows = ['8022092']
+
+# list of cows for regression analysis
+cows = ['8027107', '8022092', '8027476', '8032505', '8045911']
 
 ##################################################################
 
@@ -39,55 +45,52 @@ plot_cows = ['8027107']
 
 # can also select the specific dates we wish to plot via single_day()
 total_date_list = pd.date_range(datetime(2018, 10, 19), periods=75).tolist()
-# dates of heat taken from paper
-date_set = [date.strftime("%d-%b-%Y") for date in total_date_list[16:20]]
-#date_set = [date.strftime("%d-%b-%Y") for date in total_date_list[32:36]]
-#date_set = [date.strftime("%d-%b-%Y") for date in total_date_list[32:42]]
+# all dates to be plotted consecutively for all time series data
+date_set = [date.strftime("%d-%b-%Y") for date in total_date_list[1:-1]]
 # plot consecutive days over extended period of time
 plot_consecutive = True
 
 # select state indeces
-state_indeces = [4]
+state_indeces = [1]
 
 ##################################################################
 
-########### HOT VS NON-HOT DAY TRENDS #############
+############## GENERATE ALL DATA FOR REGRESSION ANALYSIS #################
 
-ave_day_heat, ave_day_other = hot_day_trends(plot_cows, cow_category, state_indeces, True)
-# extrapolate to length of signal
-#hot_days_ex, other_days_ex = extrapolate_heat(int(len(signal)/24), ave_day_heat, ave_day_other)
+excel_data = []
+for index in state_indeces:
+    for plot_cow in cows:
+        # Extract raw data for current cow
+        cow_data = [state_data[state_indeces[0]]+" raw"]
+        cow_data.append(plot_cow)
+        signal = single_day_trends([plot_cow], plot_cow, state_indeces, date_set, plot_consecutive, True)
+        cow_data += signal
+        excel_data.append(cow_data)
 
-####################################################
+        # IIR LP filter
+        cow_data = [state_data[state_indeces[0]] + " filtered"]
+        cow_data.append(plot_cow)
+        filtered_signal = butter_lp_filter([4.5], [4], signal, plot_cow)
+        cow_data += list(filtered_signal)
+        excel_data.append(cow_data)
 
+    # extract herd behaviour
+    cow_data = [state_data[state_indeces[0]] + " raw"]
+    cow_data.append("All")
+    all_cows = single_day_trends(None, "All", state_indeces, date_set, plot_consecutive, True)
+    cow_data+=all_cows
+    excel_data.append(cow_data)
+    # filtered herd behaviour
+    cow_data = [state_data[state_indeces[0]] + " filtered"]
+    cow_data.append("All")
+    filtered_herd = butter_lp_filter([4.5], [4], all_cows, 'All')
+    cow_data += list(filtered_herd)
+    excel_data.append(cow_data)
 
-############### SINGLE DAY ANALYSIS ################
-
-signal = single_day_trends(plot_cows, cow_category, state_indeces, date_set, plot_consecutive, True)
-hot_days = single_day_trends(None, "All", state_indeces, date_set, plot_consecutive, True)
-#other_days = single_day_trends(None, "All", state_indeces, total_date_list[24:28], plot_consecutive)
-
-####################################################
-
-################ FILTERING ANALYSIS ##################
-
-# WAVELET
-# DWT_level_2(hot_days, "hot", other_days, "other", signal, "signal")
-
-# FFT
-#fourier_transform(signal, cow_category)
-#fourier_transform(hot_days, "All Animals")
-#fourier_transform(hot_days_ex, "Hot Day Average")
-
-# FIR LP
-# cutoffs = [i+0.5 for i in range(1,6,2)]
-# widths = [4,8]
-# fil_lp_filter(cutoffs, widths, signal, cow_category)
-# filtered_signal = fil_lp_filter([3], [7], signal, cow_category)
-# fil_lp_filter([4], [7], hot_days, 'All')
-
-# IIR LP
-filtered_signal = butter_lp_filter([4], [4], signal, cow_category)
-filtered_herd = butter_lp_filter([4], [4], hot_days, 'All')
+column_headers = ['Data Type', 'Cow']
+column_headers += [x for x in range(1,1753)]
+regression_df = pd.DataFrame(excel_data, columns=column_headers)
+regression_df.to_csv(state_data[state_indeces[0]] + "_regression.csv", index = False)
 
 ####################################################
 
@@ -100,12 +103,12 @@ filtered_herd = butter_lp_filter([4], [4], hot_days, 'All')
 # area_animal = area_under_graph(ave_day_other)
 # print(area_animal)
 
-herd_comp, animal_ave_comp = heat_stress_comp(filtered_signal, filtered_herd, ave_day_other)
+# herd_comp, animal_ave_comp = heat_stress_comp(filtered_signal, filtered_herd, ave_day_other)
 # inverse when running for resting!
 # herd_comp = [1/x for x in herd_comp]
 # animal_ave_comp = [1/x for x in animal_ave_comp]
-print(herd_comp)
-print(animal_ave_comp)
+# print(herd_comp)
+# print(animal_ave_comp)
 
 ####################################################
 
