62f42304145ebcee97f5efe34d01fbc2c658d193
nhcha6 <nhcha6@student.monash.edu>
2021-01-11 13:00:17 +1100

    regressions are running with customisable input/output. Need to run on more data to get better confidence for longer term trends

--- Regressions/regression_master.py
+++ Regressions/regression_master.py
@@ -54,7 +54,8 @@ date_set = [date.strftime("%d-%b-%Y") for date in total_date_list[1:-1]]
 plot_consecutive = True
 
 # select state indeces
-state_indeces = [8]
+state_indeces = [1]
+cutoff = 4.5
 
 ##################################################################
 
@@ -77,7 +78,7 @@ for index in state_indeces:
         # IIR LP filter
         cow_data = [state_data[state_indeces[0]] + " filtered"]
         cow_data.append(plot_cow)
-        filtered_signal = butter_lp_filter([3], [4], signal, plot_cow)
+        filtered_signal = butter_lp_filter([cutoff], [4], signal, plot_cow)
         cow_data += list(filtered_signal)
         excel_data.append(cow_data)
 
@@ -90,7 +91,7 @@ for index in state_indeces:
     # filtered herd behaviour
     cow_data = [state_data[state_indeces[0]] + " filtered"]
     cow_data.append("All")
-    filtered_herd = butter_lp_filter([3], [4], all_cows, 'All')
+    filtered_herd = butter_lp_filter([cutoff], [4], all_cows, 'All')
     cow_data += list(filtered_herd)
     excel_data.append(cow_data)
 
@@ -120,4 +121,4 @@ regression_df.to_csv("Entire Dataset Output/" + state_data[state_indeces[0]] + "
 ####################################################
 
 # show plots
-plt.show()
+#plt.show()
--- Regressions/regression_models.py
+++ Regressions/regression_models.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("Five Animal Output/panting_regression.csv")
 
@@ -15,16 +16,38 @@ data_type_list = sorted(data_type_list)
 
 # iterate through each animal and data type
 for cow in cow_list:
+    if cow=='All':
+        continue
     for i in range(1,1657, 24):
         print(cow)
         print(i)
         rows = df.loc[(df["Cow"]==cow)]
         x_data = rows[[str(j) for j in range(i,i+96)]].values.tolist()
-        x_data = x_data[0]+x_data[1]
-        y_data = rows.iloc[1][str(i+96)]
+        x_data = x_data[1]+x_data[0]
+        y_data = rows.iloc[1][str(i+102)]
         x.append(x_data)
         y.append(y_data)
 
-print(np.shape(np.array(x)))
-print(np.shape(np.array(y)))
+x = np.array(x)
+y = np.array(y)
+print(np.shape(x))
+print(np.shape(y))
+print(x[0])
+print(y[0])
 
+X = sm.add_constant(x)
+results = sm.OLS(y,X).fit()
+
+print(results.summary())
+
+
+coeffs = results.params
+pvals = results.pvalues
+
+fig, ax1 = plt.subplots()
+ax1.plot(abs(coeffs))
+ax2 = ax1.twinx()
+ax2.plot(pvals, 'ro')
+
+fig.tight_layout()
+plt.show()
